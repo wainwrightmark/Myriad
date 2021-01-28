d0f6d80e99b07ddd316a591cd5bbe65c2da54a79
Mark Wainwright <wainwrightml@gmail.com>
2021-01-28 08:45:31 +0000

    Improved UI

--- Moggle/MoggleState.cs
+++ Moggle/MoggleState.cs
@@ -26,7 +26,7 @@ public record MoggleState(
     ImmutableHashSet<string> DisabledWords)
 {
     public static readonly MoggleState DefaultState = new(
-        MoggleBoard.Create(true, 4,4),
+        MoggleBoard.Create(true, 4, 4),
         null,
         0,
         ImmutableList<Coordinate>.Empty,
@@ -34,9 +34,13 @@ public record MoggleState(
         ImmutableHashSet<string>.Empty
     );
 
-    public MoggleState StartNewGame(string seed, int width, int height, bool classic, int duration)
+    public MoggleState StartNewGame(
+        string seed,
+        int width,
+        int height,
+        bool classic,
+        int duration)
     {
-
         var newState = new MoggleState(
             MoggleBoard.Create(classic, width, height).Randomize(seed),
             DateTime.Now.AddSeconds(duration),
@@ -63,40 +67,48 @@ public record MoggleState(
 
         if (ChosenPositions.Last().Equals(coordinate))
         {
-            if (ChosenPositions.Count >= 3) //Complete a word
+            switch (ChosenPositions.Count)
             {
-                var word =
-                    new string(
-                        ChosenPositions.Select(GetLetterAtCoordinate)
-                            .ToArray()
-                    );
-
-                return this with
+                //Complete a word
+                case >= 3:
                 {
-                    ChosenPositions = ImmutableList<Coordinate>.Empty,
-                    FoundWords = FoundWords.Add(word)
-                };
+                    var word =
+                        new string(
+                            ChosenPositions.Select(GetLetterAtCoordinate)
+                                .ToArray()
+                        );
+
+                    return this with
+                    {
+                        ChosenPositions = ImmutableList<Coordinate>.Empty,
+                        FoundWords = FoundWords.Add(word)
+                    };
+                }
+                //Give up on this path
+                case 1 or 2:  return this with { ChosenPositions = ImmutableList<Coordinate>.Empty };
+                default: return null; //Do nothing
             }
-            else if (ChosenPositions.Count == 1) //Give up on this path
-                return this with { ChosenPositions = ImmutableList<Coordinate>.Empty };
-            else
-                return null; //Do nothing
         }
 
         var index = ChosenPositions.LastIndexOf(coordinate);
 
-        if (index >= 0) //Go back some number of steps
-            return this with
-            {
-                ChosenPositions = ChosenPositions.Take(index + 1).ToImmutableList()
-            };
+        switch (index)
+        {
+            //Give up on this path
+            case 0 or 1: return this with { ChosenPositions = ImmutableList<Coordinate>.Empty };
+            //Go back some number of steps
+            case > 1:
+                return this with
+                {
+                    ChosenPositions = ChosenPositions.Take(index + 1).ToImmutableList()
+                };
+        }
 
         if (ChosenPositions.Last().IsAdjacent(coordinate)) //add this coordinate to the list
             return this with { ChosenPositions = ChosenPositions.Add(coordinate) };
 
         return null;
     }
-
 
     public char GetLetterAtCoordinate(Coordinate coordinate)
     {
@@ -114,13 +126,25 @@ public record MoggleState(
             {
                 switch (s.Length)
                 {
-                    case <3:  break;
-                    case 3:   total += 1;break;
-                    case 4:   total += 1;break;
-                    case 5:   total += 2;break;
-                    case 6:   total += 3;break;
-                    case 7:   total += 4;break;
-                    case >=8: total += 11;break;
+                    case <3: break;
+                    case 3:
+                        total += 1;
+                        break;
+                    case 4:
+                        total += 1;
+                        break;
+                    case 5:
+                        total += 2;
+                        break;
+                    case 6:
+                        total += 3;
+                        break;
+                    case 7:
+                        total += 4;
+                        break;
+                    case >=8:
+                        total += 11;
+                        break;
                 }
             }
 
